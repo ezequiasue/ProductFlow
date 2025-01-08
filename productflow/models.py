fb8dd250f0905b1692d33edf266a90095a8f7069
Ezequias S. Santos <arpasistema@gmail.com>
2025-01-08 16:29:49 -0400

    Update models.py

--- productflow/models.py
+++ productflow/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# Modelo de Categoria (opcional, se você precisar categorizar produtos)
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
+# Modelo de Fornecedor
+class Fornecedor(models.Model):
+    nome = models.CharField(max_length=255)
+    cnpj = models.CharField(max_length=14)
+    telefone = models.CharField(max_length=15)
+    endereco = models.TextField()
+
+    def __str__(self):
+        return self.nome
+    
+# Modelo de Produto --ORM
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade_em_estoque = models.PositiveIntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
